dfc09b9597827155aa24b03d4f85b431d6feb0a6
jeeva <jeejeeva>
2022-10-12 18:57:03 +0530

    Student mark list

--- Student.java
+++ Student.java
@@ -1,7 +1,6 @@
 class Student
 {
     int tamil, english, maths;
-
 Student (int tamil, int english, int maths)
 {
     this.tamil = tamil;
@@ -10,19 +9,15 @@ Student (int tamil, int english, int maths)
 }
 public static void main(String[] args)
 {
-    Student mark = new Student(74, 60, 70);
-    mark.student_mark();
 
-
+    Student mark = new Student (80, 65, 70);
+    mark.student_mark ();
 }
-void student_mark()
+void student_mark ()
 {
     System.out.println("Tamil = "+ tamil);
     System.out.println("English = "+english);
-    System.out.println("Maths = "+maths);
-
-
-
-
+    System.out.println("Maths = "+ maths);
 }
+
 }
