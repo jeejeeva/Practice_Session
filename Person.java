24ac2e5ea058c38a31f5d3d83e17bbaa0cc602f7
jeeva <jeejeeva>
2022-10-14 17:11:34 +0530

    One person details

---
+++ Person.java
@@ -0,0 +1,14 @@
+package tamilNadu.mohai;
+public class Person
+{
+
+public static void main(String[] args)
+{
+    Area person = new Area("venkiy", 21, "26/12/1999", "Deplamo");
+    person.mettu_street();   
+
+
+}
+
+
+}
